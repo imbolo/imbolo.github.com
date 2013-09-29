8fba3834aee4f1c7230e4a44119ccaee16587f07
imbolo <imbolo@qq.com>
2013-09-29 15:15:59 +0800

    update

--- assets/JS/job.js
+++ assets/JS/job.js
@@ -1,7 +1,9 @@
 (function(){
-	var str = "本人目前大四，自学能力极强，开发工作上手非常快。"
-	str += "\n热衷前端开发，对JS模块化开发很有兴趣，有上线单页面应用经验。对hybrid app和air等跨平台移动开发非常熟悉。";
-	str += "\n\n写的了代码，切的了图，还卖得一手好萌，实乃UX、PM之必备好伙伴。"
-	str += "\n不要犹豫了\n赶紧写起email联系我吧：imbolo@qq.com";
-	console.log(str)
+	window.onload = function() {
+		var str = "本人目前大四，自学能力极强，开发工作上手非常快。"
+		str += "\n热衷前端开发，对JS模块化开发很有兴趣，有上线单页面应用经验。对hybrid app和air等跨平台移动开发非常熟悉。";
+		str += "\n\n写的了代码，切的了图，还卖得一手好萌，实乃UX、PM之必备好伙伴。"
+		str += "\n不要犹豫了\n赶紧写起email联系我吧：imbolo@qq.com";
+		console.log(str)
+	}
 })();
